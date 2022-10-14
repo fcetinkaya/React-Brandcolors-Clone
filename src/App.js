2bd8ca2ed4d0a041e468c0cbf3c1e103b7cb6326
fcetinkaya <fatihxcetinkaya@gmail.com>
2022-10-15 00:09:23 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import MainContext from './MainContext';
+import Sidebar from './components/Sidebar';
+import Content from './components/Content';
+import BrandsData from './brands.json'
+import Copied from './components/Copied'
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Collection from "./components/Collection";
+import {forceCheck} from "react-lazyload"
 
 function App() {
+
+  const brandsArray = []
+  Object.keys(BrandsData).map(key => {
+    brandsArray.push(BrandsData[key])
+  })
+
+  const [brands, setBrands] = useState(brandsArray)
+  const [selectedBrands, setSelectedBrands] = useState([])
+  const [copied, setCopied] = useState(false)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    if (copied) {
+      const timeout = setTimeout(() => {
+        setCopied(false)
+      }, 1000)
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+  })
+
+  useEffect(() => {
+    setBrands(brandsArray.filter(brand => brand.title.toLowerCase().includes(search)))
+  }, [search])
+
+useEffect(()=>{
+  forceCheck()
+},[brands])
+
+  const data = {
+    brands,
+    setSelectedBrands,
+    selectedBrands,
+    setCopied,
+    search,
+    setSearch
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <MainContext.Provider value={data}>
+        {copied && <Copied color={copied} />}
+        <Sidebar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Content />}></Route>
+            <Route path="/collection/:slugs" element={<Collection />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </MainContext.Provider>
+    </>
   );
 }
 
---
+++ src/components/Brand.js
@@ -0,0 +1,40 @@
+import { useContext } from 'react'
+import { getContrastYIQ } from '../Helpers'
+import MainContext from '../MainContext'
+import ClipboardButton from 'react-clipboard.js'
+
+
+
+function Brand({ brand,style }) {
+    const { setSelectedBrands, selectedBrands, setCopied } = useContext(MainContext)
+
+    const toggleSelected = () => {
+        if (selectedBrands.includes(brand.slug)) {
+            setSelectedBrands(selectedBrands.filter(slug => slug !== brand.slug))
+        } else {
+            setSelectedBrands([...selectedBrands, brand.slug])
+        }
+    }
+
+    const setColor = (color) => {
+        setCopied(color)
+    }
+
+    return (
+        <div style={style}>
+            <div className={`brand ${selectedBrands.includes(brand.slug) ? 'selected' : ''}`}>
+                <h5 onClick={toggleSelected}>{brand.title}</h5>
+                <div className="brand-colors">
+                    {brand.colors.map(color => (
+                        <ClipboardButton data-clipboard-text={color} onSuccess={() => setColor(color)} component="span" style={{ '--bgColor': `#${color}`, '--textColor': `${getContrastYIQ(color)}` }}>
+                            {color}
+                        </ClipboardButton>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Brand
